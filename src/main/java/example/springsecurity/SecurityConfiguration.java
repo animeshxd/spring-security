4526ebe6588bfab830a547774abcf2a13b4a27bc
Animesh Murmu <am2646374@gmail.com>
2022-09-18 17:01:18 +0530

    Configuring basic spring security with jdbc api - UserDetailsManager, DataSource, JdbcUserDetailsManager, EmbeddedDatabaseBuilder,

--- src/main/java/example/springsecurity/SecurityConfiguration.java
+++ src/main/java/example/springsecurity/SecurityConfiguration.java
@@ -1,18 +1,35 @@
 package example.springsecurity;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
 
+    @Bean
+    public DataSource dataSource(){
+        return new EmbeddedDatabaseBuilder()
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .setType(EmbeddedDatabaseType.H2)
+                .build();
+    }
+    
+
     @Bean 
-    public InMemoryUserDetailsManager userDetailsManager(){
+    public UserDetailsManager userDetailsManager(){
+
         UserDetails user = 
             User.withDefaultPasswordEncoder()
                 .username("user")
@@ -26,7 +43,11 @@ public class SecurityConfiguration {
                 .password("admin")
                 .roles("ADMIN")
                 .build();
-        return new InMemoryUserDetailsManager(user, admin);
+        
+        var userDetailsManager = new JdbcUserDetailsManager(dataSource());
+        userDetailsManager.createUser(user);
+        userDetailsManager.createUser(admin);
+        return userDetailsManager;
     }
 
     @Bean
