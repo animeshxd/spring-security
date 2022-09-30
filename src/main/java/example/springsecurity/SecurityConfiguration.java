fce90820a843d66b5db0914c5d4f220b5cd79614
Animesh Murmu <am2646374@gmail.com>
2022-09-30 22:14:50 +0530

    more basic

--- src/main/java/example/springsecurity/SecurityConfiguration.java
+++ src/main/java/example/springsecurity/SecurityConfiguration.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -12,16 +15,16 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
     @Bean 
-    public InMemoryUserDetailsManager userDetailsManager(){
+    public UserDetailsService userDetailsManager(){
         UserDetails user = 
-            User.withDefaultPasswordEncoder()
+            User.builder()
                 .username("user")
                 .password("user")
                 .roles("USER")
                 .build();
         
         UserDetails admin = 
-            User.withDefaultPasswordEncoder()
+            User.builder()
                 .username("admin")
                 .password("admin")
                 .roles("ADMIN")
@@ -40,6 +43,9 @@ public class SecurityConfiguration {
 
         return http.build();
     }
-        
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
     
 }
